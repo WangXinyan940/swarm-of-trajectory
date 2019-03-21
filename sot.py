cdefff9372a3458649ec54594fe40223aa85bcb2
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-03-21 17:31:33 +0800

    Create sot.py

---
+++ sot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+
+
+
+def printTitle():
+    print("""
+    ------------------------------------------------
+    | Swarm-of-Trajectory Dynamic Simulation Package
+    | 
+    | Version: 0.1
+    | Program Language: Python 3.6
+    | Developer: Xinyan Wang
+    ------------------------------------------------
+    """)
+
+def printHelp():
+
+
+def main():
+    printTitle()
+    conf = argparse()
