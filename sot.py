096edee02be8c6e1735f4553589de4a0f709bc69
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2019-03-22 13:54:15 +0800

    Update sot.py

--- sot.py
+++ sot.py
@@ -1,6 +1,7 @@
-import numpy as np
 import sys
 import json
+import numpy as np
+from jinja2 import Template
 
 
 def printTitle():
@@ -8,13 +9,14 @@ def printTitle():
     Print the title of the output result.
     """
     print("""
---------------------------------------------------
+----------------------------------------------------------------
 | Swarm-of-Trajectory Dynamic Simulation Package
 | 
 | Version: 0.1
 | Program Language: Python 3.6
 | Developer: Xinyan Wang
---------------------------------------------------
+| Homepage: https://github.com/WangXinyan940/swarm-of-trajectory
+----------------------------------------------------------------
     """)
 
 
@@ -24,20 +26,68 @@ def printHelp():
     """
     print("""
     Usage:
-        python sot.py -j json
+        python sot.py -j json -t template
     """)
+
+
+def readFile(fname, func):
+    """
+    Read text file and deal with different functions.
+    """
+    with open(fname, "r") as f:
+        text = f.readlines()
+    return func(text)
+
+
+def readXYZ(text):
+    """
+    Read xyz format text.
+    """
+    natoms = int(text[0].strip())
+    body = text[2:natoms + 2]
+    body = [i.strip().split() for i in body]
+    atom = [i[0] for i in body]
+    crd = [[float(j) for j in i[1:]] for i in body]
+    return atom, crd
+
+def genQMInput(atom, crd, temp):
+    """
+    Generate QM Input file for force calculation.
+    """
+    return temp.render(data=zip(atom, crd))
+
+
+def testTemplate(conf):
+    """
+    Test whether the template file is correct. (Use water)
+    """
+    t_atom = ["H", "O", "H"]
+    t_xyz = [[1.0, 0.0, 0.0],
+             [0.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0]]
+    template = readFile(conf["force"]["template"],
+                        lambda x: Template("".join(x)))
+    print("Generate template below:\n++++++++++++++++++++++++")
+    print(genQMInput(t_atom, t_xyz, template))
 
 
 def argparse():
     """
     Parse the args.
     """
-    if "-j" not in sys.argv:
+    # Test qm template
+    if "-j" not in sys.argv and "-t" not in sys.argv:
         printHelp()
         exit()
+
     with open(sys.argv[2], "r") as f:
         text = "".join(f)
-    return json.loads(text)
+        conf = json.loads(text)
+
+    if "-t" in sys.argv:
+        testTemplate(conf)
+        exit()
+    return conf
 
 
 def main():
